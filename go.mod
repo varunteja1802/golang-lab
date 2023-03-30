19034f32da19d3addabe40d0de9d722c5e716aac
Kalakoti Varun Teja <89351917+varunteja1802@users.noreply.github.com>
2023-03-30 17:06:00 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,22 @@
+module linearRegression
+
+go 1.20
+
+require (
+	github.com/go-gota/gota v0.12.0
+	gonum.org/v1/plot v0.9.0
+)
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20180226025133-644b8db467af // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/go-fonts/liberation v0.1.1 // indirect
+	github.com/go-latex/latex v0.0.0-20210118124228-b3d85cf34e07 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/phpdave11/gofpdf v1.4.2 // indirect
+	github.com/sajari/regression v1.0.1 // indirect
+	golang.org/x/image v0.0.0-20210216034530-4410531fe030 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+)
---
+++ hist.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-gota/gota/dataframe"
+	"github.com/sajari/regression"
+)
+
+func main() {
+	// Open the CSV file.
+	carPrice, err := os.Open("car_data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer carPrice.Close()
+
+	// Create a dataframe from the CSV file.
+	carDF := dataframe.ReadCSV(carPrice)
+
+	// Use the Describe method to calculate summary statistics
+	// for all of the columns in one shot.
+	carsSummary := carDF.Describe()
+
+	// Output the summary statistics
+	fmt.Println(carsSummary)
+
+	// Create a new regression instance.
+	r := new(regression.Regression)
+
+	// Add the independent variables to the regression.
+	for _, colName := range carDF.Names() {
+		if colName == "Selling_Price" {
+			continue
+		}
+		colData := carDF.Col(colName).Float()
+		dp := make([]regression.DataPoint, len(colData))
+		for i, f := range colData {
+			dp[i].Observed = f
+			dp[i].Variables = []float64{carDF.Col("Age").Float()[i]}
+		}
+		r.Train(dp)
+	}
+
+	// Add the dependent variable to the regression.
+	sellingPrice := carDF.Col("Selling_Price").Float()
+	dp := make([]regression.DataPoint, len(sellingPrice))
+	for i, f := range sellingPrice {
+		dp[i].Observed = f
+	}
+	r.Train(dp)
+
+	// Fit the regression using the normal equation.
+	r.Run()
+
+	// Output the regression formula.
+	formula := "Selling_Price = "
+	for i, coef := range r.Weights {
+		if i == 0 {
+			formula += fmt.Sprintf("%.2f", coef)
+		} else {
+			formula += fmt.Sprintf(" + %.2f * %s", coef, carDF.Names()[i-1])
+		}
+	}
+	fmt.Println("Regression Formula:", formula)
+
+	// Output the regression statistics.
+	fmt.Println("R²:", r.R2)
+	fmt.Println("Coefficients:", r.Weights)
+	fmt.Println("Intercept:", r.Intercept)
+
+	fmt.Println("Complete Dataframe:")
+	fmt.Println(carDF)
+}
